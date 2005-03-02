4b60d9751e1c09ad42c889f96951ccba817190a6
dmoore <dmoore>
2005-03-02 14:36:02 +0000

    2005-03-02  David Moore  <dcm@acm.org>
            * iptc-dataset.h: Fixed copyright notice

--- libiptcdata/iptc-dataset.h
+++ libiptcdata/iptc-dataset.h
@@ -1,6 +1,6 @@
 /* iptc-dataset.h
  *
- * Copyright © 2001 Lutz Müller <lutz@users.sourceforge.net>
+ * Copyright © 2005 David Moore <dcm@acm.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
